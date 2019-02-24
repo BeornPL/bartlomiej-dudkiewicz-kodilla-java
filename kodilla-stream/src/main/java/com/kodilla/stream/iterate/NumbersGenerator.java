b15ec1e23aff71975d0d1b8edeef2bb5ab6ee9c4
BeornPL <37457935+BeornPL@users.noreply.github.com>
2019-02-24 20:16:26 +0100

    8.2

--- kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
@@ -4,9 +4,14 @@ import java.util.stream.*;
 
 public final class NumbersGenerator {
     public static void generateEven(int max) {
-        Stream.iterate(1, n -> n+1)
+        Stream.iterate(1, n -> ++n)
                 .limit(max)
                 .filter(n -> n % 2 == 0)
                 .forEach(System.out::println);
     }
+
+    public static void main(String[] args) {
+        NumbersGenerator.generateEven(20);
+    }
 }
+
