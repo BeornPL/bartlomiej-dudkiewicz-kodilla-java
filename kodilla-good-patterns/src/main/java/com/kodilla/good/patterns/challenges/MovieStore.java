600741c4211402abaa8880cf19339971964e8e4f
BeornPL <37457935+BeornPL@users.noreply.github.com>
2019-03-05 22:02:58 +0100

    9.1.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -34,10 +34,12 @@ class MovieStore {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
-        movieStore.getMovies().entrySet().stream()
+        String result = movieStore.getMovies().entrySet().stream()
                 .map(entry -> entry.getValue())
                 .flatMap(list -> list.stream())
-                .forEach(s -> System.out.print(s+"!"));
+                .collect(Collectors.joining("!"));
+
+        System.out.println(result);
 
     }
 }
